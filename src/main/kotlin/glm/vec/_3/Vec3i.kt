c75aca9bd7a2253b6ea0b369f469b31e5431c9b9
Giuseppe Barbieri <elect86@gmail.com>
2017-04-04 11:16:17 +0200

    comp* for Vec*i

--- src/main/kotlin/glm/vec/_3/Vec3i.kt
+++ src/main/kotlin/glm/vec/_3/Vec3i.kt
@@ -21,6 +21,7 @@ class Vec3i(x: Int, y: Int, z: Int) : Vec3t<Int>(x, y, z) {
     constructor() : this(0)
 
     constructor(v: Vec2t<out Number>) : this(v.x, v.y, 0)
+    constructor(v: Vec2t<out Number>, z: Number) : this(v.x, v.y, z)    // TODO all combo
     constructor(v: Vec3t<out Number>) : this(v.x, v.y, v.z)
     constructor(v: Vec4t<out Number>) : this(v.x, v.y, v.z)
 
