1c62efcf7f2aa6622fc14e83e6ae603e64982d5c
gBarbieri <elect86@gmail.com>
2017-08-31 13:07:11 +0200

    vec2 cmp

--- src/main/kotlin/glm_/vec2/Vec2.kt
+++ src/main/kotlin/glm_/vec2/Vec2.kt
@@ -318,6 +318,12 @@ class Vec2(x: Float, y: Float) : Vec2t<Float>(x, y) {
         y = glm.max(y, b.y.f)
     }
 
+    infix fun lessThan(f: Float) = x < f && y < f
+    infix fun lessThanEqual(f: Float) = x <= f && y <= f
+    infix fun equal(f: Float) = x == f && y == f
+    infix fun greaterThan(f: Float) = x > f && y > f
+    infix fun greaterThanEqual(f: Float) = x >= f && y >= f
+
     infix fun dot(b: Vec2) = x * b.x + y * b.y
 
     fun length() = glm.length(this)
