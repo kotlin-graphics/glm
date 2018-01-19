dcf850ee5e6d42d7f79f9c62ca676b70640d89cf
gBarbieri <elect86@gmail.com>
2018-01-19 11:22:44 +0100

    - added missing random methods

--- src/main/kotlin/glm_/random.kt
+++ src/main/kotlin/glm_/random.kt
@@ -8,67 +8,100 @@ import glm_.vec3.Vec3d
 import glm_.vec4.Vec4
 import glm_.vec4.Vec4d
 import java.util.concurrent.ThreadLocalRandom
+import kotlin.math.acos
+import kotlin.math.cos
+import kotlin.math.sin
 
 
 interface random {
 
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
     fun linearRand(min: Float, max: Float): Float {
         var d = ThreadLocalRandom.current().nextDouble(min.d, max + Double.MIN_VALUE)
         while (d > max) d = ThreadLocalRandom.current().nextDouble(min.d, max + Double.MIN_VALUE)
         return d.f
     }
 
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
     fun linearRand(min: Double, max: Double): Double {
         var d = ThreadLocalRandom.current().nextDouble(min, max + Double.MIN_VALUE)
         while (d > max) d = ThreadLocalRandom.current().nextDouble(min, max + Double.MIN_VALUE)
         return d
     }
 
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
     fun linearRand(min: Int, max: Int): Int {
         var d = ThreadLocalRandom.current().nextInt(min, max + Int.MIN_VALUE)
         while (d > max) d = ThreadLocalRandom.current().nextInt(min, max + Int.MIN_VALUE)
         return d
     }
 
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
     fun linearRand(min: Long, max: Long): Long {
         var d = ThreadLocalRandom.current().nextLong(min, max + Long.MIN_VALUE)
         while (d > max) d = ThreadLocalRandom.current().nextLong(min, max + Long.MIN_VALUE)
         return d
     }
 
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
     fun linearRand(min: Vec2, max: Vec2, res: Vec2 = Vec2()): Vec2 {
         res.x = linearRand(min.x, max.x)
         res.y = linearRand(min.y, max.y)
         return res
     }
 
-    fun double(min: Vec2d, max: Vec2d, res: Vec2d = Vec2d()): Vec2d {
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
+    fun linearRand(min: Vec2d, max: Vec2d, res: Vec2d = Vec2d()): Vec2d {
         res.x = linearRand(min.x, max.x)
         res.y = linearRand(min.y, max.y)
         return res
     }
 
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
     fun linearRand(min: Vec2b, max: Vec2b, res: Vec2b = Vec2b()): Vec2b {
         res.x = linearRand(min.x.i, max.x.i).b
         res.y = linearRand(min.y.i, max.y.i).b
         return res
     }
 
-    fun float(min: Vec3, max: Vec3, res: Vec3 = Vec3()): Vec3 {
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
+    fun linearRand(min: Vec3, max: Vec3, res: Vec3 = Vec3()): Vec3 {
         res.x = linearRand(min.x, max.x)
         res.y = linearRand(min.y, max.y)
         res.z = linearRand(min.z, max.z)
         return res
     }
 
-    fun double(min: Vec3d, max: Vec3d, res: Vec3d = Vec3d()): Vec3d {
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
+    fun linearRand(min: Vec3d, max: Vec3d, res: Vec3d = Vec3d()): Vec3d {
         res.x = linearRand(min.x, max.x)
         res.y = linearRand(min.y, max.y)
         res.z = linearRand(min.z, max.z)
         return res
     }
 
-    fun float(min: Vec4, max: Vec4, res: Vec4 = Vec4()): Vec4 {
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
+    fun linearRand(min: Vec4, max: Vec4, res: Vec4 = Vec4()): Vec4 {
         res.x = linearRand(min.x, max.x)
         res.y = linearRand(min.y, max.y)
         res.z = linearRand(min.z, max.z)
@@ -76,7 +109,10 @@ interface random {
         return res
     }
 
-    fun double(min: Vec4d, max: Vec4d, res: Vec4d = Vec4d()): Vec4d {
+    /** Generate random numbers in the interval [min, max], according a linear distribution
+     *  @param min Minimum value included in the sampling
+     *  @param max Maximum value included in the sampling */
+    fun linearRand(min: Vec4d, max: Vec4d, res: Vec4d = Vec4d()): Vec4d {
         res.x = linearRand(min.x, max.x)
         res.y = linearRand(min.y, max.y)
         res.z = linearRand(min.z, max.z)
@@ -84,6 +120,7 @@ interface random {
         return res
     }
 
+    /** Generate random numbers in the interval [min, max], according a gaussian distribution */
     fun gaussRand(mean: Float, deviation: Float): Float {
 
         var x1 = linearRand(-1f, 1f)
@@ -99,6 +136,7 @@ interface random {
         return x2 * deviation * deviation * glm.sqrt(-2 * glm.log(w)) + mean
     }
 
+    /** Generate random numbers in the interval [min, max], according a gaussian distribution */
     fun gaussRand(mean: Double, deviation: Double): Double {
 
         var x1 = linearRand(-1.0, 1.0)
@@ -114,18 +152,21 @@ interface random {
         return x2 * deviation * deviation * glm.sqrt(-2 * glm.log(w)) + mean
     }
 
+    /** Generate random numbers in the interval [min, max], according a gaussian distribution */
     fun gaussRand(mean: Vec2, deviation: Vec2, res: Vec2 = Vec2()): Vec2 {
         res.x = gaussRand(mean.x, deviation.x)
         res.y = gaussRand(mean.y, deviation.y)
         return res
     }
 
+    /** Generate random numbers in the interval [min, max], according a gaussian distribution */
     fun gaussRand(mean: Vec2d, deviation: Vec2d, res: Vec2d = Vec2d()): Vec2d {
         res.x = gaussRand(mean.x, deviation.x)
         res.y = gaussRand(mean.y, deviation.y)
         return res
     }
 
+    /** Generate random numbers in the interval [min, max], according a gaussian distribution */
     fun gaussRand(mean: Vec3, deviation: Vec3, res: Vec3 = Vec3()): Vec3 {
         res.x = gaussRand(mean.x, deviation.x)
         res.y = gaussRand(mean.y, deviation.y)
@@ -133,6 +174,7 @@ interface random {
         return res
     }
 
+    /** Generate random numbers in the interval [min, max], according a gaussian distribution */
     fun gaussRand(mean: Vec3d, deviation: Vec3d, res: Vec3d = Vec3d()): Vec3d {
         res.x = gaussRand(mean.x, deviation.x)
         res.y = gaussRand(mean.y, deviation.y)
@@ -140,6 +182,7 @@ interface random {
         return res
     }
 
+    /** Generate random numbers in the interval [min, max], according a gaussian distribution */
     fun gaussRand(mean: Vec4, deviation: Vec4, res: Vec4 = Vec4()): Vec4 {
         res.x = gaussRand(mean.x, deviation.x)
         res.y = gaussRand(mean.y, deviation.y)
@@ -148,6 +191,7 @@ interface random {
         return res
     }
 
+    /** Generate random numbers in the interval [min, max], according a gaussian distribution */
     fun gaussRand(mean: Vec4d, deviation: Vec4d, res: Vec4d = Vec4d()): Vec4d {
         res.x = gaussRand(mean.x, deviation.x)
         res.y = gaussRand(mean.y, deviation.y)
@@ -156,6 +200,53 @@ interface random {
         return res
     }
 
+    /** Generate a random 2D vector which coordinates are regulary distributed on a circle of a given radius */
+    fun circularRand(radius: Float, res: Vec2 = Vec2()): Vec2 {
+        val a = linearRand(0f, 6.283185307179586476925286766559f)
+        res.put(glm.cos(a), glm.sin(a))
+        res *= radius
+        return res
+    }
+
+    /** Generate a random 2D vector which coordinates are regulary distributed on a circle of a given radius */
+    fun circularRand(radius: Double, res: Vec2d = Vec2d()): Vec2d {
+        val a = linearRand(0.0, 6.283185307179586476925286766559)
+        res.put(glm.cos(a), glm.sin(a))
+        res *= radius
+        return res
+    }
+
+    /** Generate a random 3D vector which coordinates are regulary distributed on a sphere of a given radius */
+    fun sphericalRand(radius: Float, res: Vec3 = Vec3()): Vec3 {
+        val theta = linearRand(0f, 6.283185307179586476925286766559f)
+        val phi = acos(linearRand(-1f, 1f))
+
+        val sinPhi = sin(phi)
+        val x = sinPhi * cos(theta)
+        val y = sinPhi * sin(theta)
+        val z = cos(phi)
+
+        res.put(x, y, z)
+        res *= radius
+        return res
+    }
+
+    /** Generate a random 3D vector which coordinates are regulary distributed on a sphere of a given radius */
+    fun sphericalRand(radius: Double, res: Vec3d = Vec3d()): Vec3d {
+        val theta = linearRand(0.0, 6.283185307179586476925286766559)
+        val phi = acos(linearRand(-1.0, 1.0))
+
+        val sinPhi = sin(phi)
+        val x = sinPhi * cos(theta)
+        val y = sinPhi * sin(theta)
+        val z = cos(phi)
+
+        res.put(x, y, z)
+        res *= radius
+        return res
+    }
+
+    /** Generate a random 2D vector which coordinates are regulary distributed within the area of a disk of a given radius */
     fun diskRand(radius: Float, res: Vec2 = Vec2()): Vec2 {
 
         res.x = linearRand(-radius, radius)
@@ -170,6 +261,7 @@ interface random {
         return res
     }
 
+    /** Generate a random 2D vector which coordinates are regulary distributed within the area of a disk of a given radius */
     fun diskRand(radius: Double, res: Vec2d = Vec2d()): Vec2d {
 
         res.x = linearRand(-radius, radius)
@@ -184,6 +276,7 @@ interface random {
         return res
     }
 
+    /** Generate a random 2D vector which coordinates are regulary distributed within the area of a disk of a given radius */
     fun diskRand(radius: Float, res: Vec3 = Vec3()): Vec3 {
 
         res.x = linearRand(-radius, radius)
@@ -200,6 +293,7 @@ interface random {
         return res
     }
 
+    /** Generate a random 2D vector which coordinates are regulary distributed within the area of a disk of a given radius */
     fun diskRand(radius: Double, res: Vec3d = Vec3d()): Vec3d {
 
         res.x = linearRand(-radius, radius)
@@ -216,6 +310,7 @@ interface random {
         return res
     }
 
+    /** Generate a random 2D vector which coordinates are regulary distributed within the area of a disk of a given radius */
     fun diskRand(radius: Float, res: Vec4 = Vec4()): Vec4 {
 
         res.x = linearRand(-radius, radius)
@@ -234,6 +329,7 @@ interface random {
         return res
     }
 
+    /** Generate a random 2D vector which coordinates are regulary distributed within the area of a disk of a given radius */
     fun diskRand(radius: Double, res: Vec4d = Vec4d()): Vec4d {
 
         res.x = linearRand(-radius, radius)
@@ -252,6 +348,32 @@ interface random {
         return res
     }
 
-    //TODO sphericalRand
+    /** Generate a random 3D vector which coordinates are regulary distributed within the volume of a ball of a given radius */
+    fun ballRand(radius: Float, res: Vec3 = Vec3()): Vec3 {
+        res put 0f
+        var lenRadius: Float
+
+        do {
+            res put linearRand(Vec3(-radius), Vec3(radius))
+            lenRadius = res.length
+        }
+        while(lenRadius > radius)
+
+        return res
+    }
+
+    /** Generate a random 3D vector which coordinates are regulary distributed within the volume of a ball of a given radius */
+    fun ballRand(radius: Double, res: Vec3d = Vec3d()): Vec3d {
+        res put 0.0
+        var lenRadius: Double
+
+        do {
+            res put linearRand(Vec3d(-radius), Vec3d(radius))
+            lenRadius = res.length
+        }
+        while(lenRadius > radius)
+
+        return res
+    }
 }
 
