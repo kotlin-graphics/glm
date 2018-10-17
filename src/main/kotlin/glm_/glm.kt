449bdbcf7052d3e3fc60fa76b4b852f887b52252
elect86 <elect86@gmail.com>
2018-10-17 18:57:33 +0200

    Testing Clojure interoperability

--- src/main/kotlin/glm_/glm.kt
+++ src/main/kotlin/glm_/glm.kt
@@ -140,6 +140,14 @@ class Java {
     }
 }
 
+class Clojure {
+
+    companion object {
+        @JvmStatic
+        fun frustum(left: Float, right: Float, bottom: Float, top: Float, near: Float, far: Float) = glm.frustum(left, right, bottom, top, near, far)
+    }
+}
+
 typealias uint = Int
 typealias ulong = Long
 
