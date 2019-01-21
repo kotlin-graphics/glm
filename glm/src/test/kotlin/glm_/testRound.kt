68e68f3bb1103788ae102268f5f9687edbdd6b6f
gbarbieri <elect86@gmail.com>
2019-01-21 13:19:45 +0100

    - bumped kotlintest, solved dirty path issue

--- glm/src/test/kotlin/glm_/testRound.kt
+++ glm/src/test/kotlin/glm_/testRound.kt
@@ -11,43 +11,40 @@ class testRound : StringSpec() {
 
     init {
 
-        "ceil multiple" {
+        "ceil multiple - test float" {
 
-            "test float" {
+            class Data(val source: Double, val multiple: Double, val return_: Double, val epsilon: Double)
 
-                class Data(val source: Double, val multiple: Double, val return_: Double, val epsilon: Double)
+            val data = arrayOf(
+                    Data(3.4, 0.3, 3.6, 0.0001),
+                    Data(-1.4, 0.3, -1.2, 0.0001))
 
-                val data = arrayOf(
-                        Data(3.4, 0.3, 3.6, 0.0001),
-                        Data(-1.4, 0.3, -1.2, 0.0001))
+            data.forEach {
 
-                data.forEach {
-
-                    val result = glm.ceilMultiple(it.source, it.multiple)
-                    glm.epsilonEqual(it.return_, result, it.epsilon) shouldBe true
-                }
+                val result = glm.ceilMultiple(it.source, it.multiple)
+                glm.epsilonEqual(it.return_, result, it.epsilon) shouldBe true
             }
+        }
 
-            "test int" {
+        "ceil multiple - test int" {
 
-                class Data(val source: Int, val multiple: Int, val return_: Int, val epsilon: Int)
+            class Data(val source: Int, val multiple: Int, val return_: Int, val epsilon: Int)
 
-                val data = arrayOf(
-                        Data(3, 4, 4, 0),
-                        Data(7, 4, 8, 0),
-                        Data(5, 4, 8, 0),
-                        Data(1, 4, 4, 0),
-                        Data(1, 3, 3, 0),
-                        Data(4, 3, 6, 0),
-                        Data(4, 1, 4, 0),
-                        Data(1, 1, 1, 0),
-                        Data(7, 1, 7, 0))
+            val data = arrayOf(
+                    Data(3, 4, 4, 0),
+                    Data(7, 4, 8, 0),
+                    Data(5, 4, 8, 0),
+                    Data(1, 4, 4, 0),
+                    Data(1, 3, 3, 0),
+                    Data(4, 3, 6, 0),
+                    Data(4, 1, 4, 0),
+                    Data(1, 1, 1, 0),
+                    Data(7, 1, 7, 0))
 
-                data.forEach {
+            data.forEach {
 
-                    val result = glm.ceilMultiple(it.source, it.multiple)
-                    glm.epsilonEqual(it.return_, result, it.epsilon) shouldBe true
-                }
+                val result = glm.ceilMultiple(it.source, it.multiple)
+                it.return_ shouldBe result
             }
         }
     }
