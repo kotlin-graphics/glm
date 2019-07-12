a282ff55a4c0ff2589c56380e147afe01052aa5f
gbarbieri <elect86@gmail.com>
2019-07-12 11:32:53 +0200

    - kotlin 1.3.41 - gradle 5.5.1 - shadow 5.1.0 - kotlintest 3.3.3 - lwjgl 3.2.2 - java JPMS

---
+++ glm/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.github.kotlin_graphics.glm {
+
+    requires kotlin.stdlib;
+
+    requires org.lwjgl;
+
+    requires com.github.kotlin_graphics.kool;
+
+    requires java.desktop;
+
+    exports glm_;
+}
--- glm/src/main/kotlin/glm_/vec2/Vec2.kt
+++ glm/src/main/kotlin/glm_/vec2/Vec2.kt
@@ -7,10 +7,8 @@ import glm_.vec3.Vec3t
 import glm_.vec4.Vec4bool
 import glm_.vec4.Vec4t
 import kool.Ptr
-import kool.FloatBuffer
 import kool.pos
 import kool.set
-import org.lwjgl.system.MemoryStack
 import org.lwjgl.system.MemoryUtil.memGetFloat
 import org.lwjgl.system.MemoryUtil.memPutFloat
 import java.awt.Color
