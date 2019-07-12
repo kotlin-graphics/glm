852608b2b2ec19ea4627ba22fd6c62f25dd2b1ed
gbarbieri <elect86@gmail.com>
2019-07-12 13:33:59 +0200

    all packages exported

--- glm/src/main/java/module-info.java
+++ glm/src/main/java/module-info.java
@@ -9,4 +9,38 @@ open module com.github.kotlin_graphics.glm {
     requires java.desktop;
 
     exports glm_;
+    exports glm_.detail;
+    exports glm_.dualQuat;
+    exports glm_.func;
+    exports glm_.func.common;
+    exports glm_.gtc;
+    exports glm_.gtc.matrixTransform;
+    exports glm_.gtx;
+    exports glm_.mat2x2;
+    exports glm_.mat2x2.operators;
+    exports glm_.mat2x3;
+//    exports glm_.mat2x3.operators;
+    exports glm_.mat2x4;
+//    exports glm_.mat2x4.operators;
+    exports glm_.mat3x2;
+//    exports glm_.mat3x2.operators;
+    exports glm_.mat3x3;
+    exports glm_.mat3x3.operators;
+    exports glm_.mat3x4;
+//    exports glm_.mat3x4.operators;
+    exports glm_.mat4x2;
+//    exports glm_.mat4x2.operators;
+    exports glm_.mat4x3;
+//    exports glm_.mat4x3.operators;
+    exports glm_.mat4x4;
+    exports glm_.mat4x4.operators;
+    exports glm_.quat;
+    exports glm_.vec1;
+    exports glm_.vec1.operators;
+    exports glm_.vec2;
+    exports glm_.vec2.operators;
+    exports glm_.vec3;
+    exports glm_.vec3.operators;
+    exports glm_.vec4;
+    exports glm_.vec4.operators;
 }
