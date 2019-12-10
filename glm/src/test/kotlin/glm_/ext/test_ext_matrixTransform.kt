0d3697c946601fa4e8f45dc7316131585a00380c
gbarbieri <elect86@gmail.com>
2019-12-10 10:49:52 +0100

    :construction:  Added rotate test

--- glm/src/test/kotlin/glm_/ext/test_ext_matrixTransform.kt
+++ glm/src/test/kotlin/glm_/ext/test_ext_matrixTransform.kt
@@ -1,5 +1,7 @@
 package glm_.ext
 
+import glm_.func.rad
+import glm_.glm
 import glm_.mat4x4.Mat4
 import glm_.shouldEqual
 import glm_.vec3.Vec3
@@ -18,7 +20,7 @@ class test_ext_matrixTransform : StringSpec() {
             T[3] shouldEqual Vec4(1f)
         }
 
-        "scale"        {
+        "scale" {
 
             val M = Mat4(1f)
             val V = Vec3(2f)
@@ -30,6 +32,15 @@ class test_ext_matrixTransform : StringSpec() {
                     0, 0, 2, 0,
                     0, 0, 0, 1)
             S shouldEqual R
+        }
+
+        "rotate" {
+
+            val A = Vec4(1f, 0f, 0f, 1f)
+
+            val R = glm.rotate(Mat4(1f), 90f.rad, Vec3(0, 0, 1))
+            val B = R * A
+            B.shouldEqual(Vec4(0f, 1f, 0f, 1f), 0.0001f)
         }
     }
 }
