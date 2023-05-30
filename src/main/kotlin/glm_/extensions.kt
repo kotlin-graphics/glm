bd1a575466dab30311bdd36d549d26d9942a74c4
Bixilon <bixilon@bixilon.de>
2023-05-30 15:52:44 +0200

    add primitive short casts
    This massively reduces testing time (almost half it now)

--- src/main/kotlin/glm_/extensions.kt
+++ src/main/kotlin/glm_/extensions.kt
@@ -10,50 +10,127 @@ import kotlin.math.pow
  * Created by GBarbieri on 07.12.2016.
  */
 
-val Number.f get() = toFloat()
-val Number.b get() = toByte()
-val Number.d get() = toDouble()
-val Number.i get() = toInt()
-val Number.L get() = toLong()
-val Number.s get() = toShort()
-val Number.ub get() = toUbyte()
-val Number.ui get() = toUint()
-val Number.ul get() = toUlong()
-val Number.us get() = toUshort()
-val Number.c get() = toChar()
-val Number.bool get() = i != 0
+inline val Byte.f get() = toFloat()
+inline val Byte.b get() = toByte()
+inline val Byte.d get() = toDouble()
+inline val Byte.i get() = toInt()
+inline val Byte.L get() = toLong()
+inline val Byte.s get() = toShort()
+inline val Byte.ub get() = toUbyte()
+inline val Byte.ui get() = toUint()
+inline val Byte.ul get() = toUlong()
+inline val Byte.us get() = toUshort()
+inline val Byte.c get() = toInt().toChar()
+inline val Byte.bool get() = i != 0
+
+inline val Short.f get() = toFloat()
+inline val Short.b get() = toByte()
+inline val Short.d get() = toDouble()
+inline val Short.i get() = toInt()
+inline val Short.L get() = toLong()
+inline val Short.s get() = toShort()
+inline val Short.ub get() = toUbyte()
+inline val Short.ui get() = toUint()
+inline val Short.ul get() = toUlong()
+inline val Short.us get() = toUshort()
+inline val Short.c get() = toInt().toChar()
+inline val Short.bool get() = i != 0
+
+inline val Char.f get() = code.toFloat()
+inline val Char.b get() = code.toByte()
+inline val Char.d get() = code.toDouble()
+inline val Char.i get() = code
+inline val Char.L get() = code.toLong()
+inline val Char.s get() = code.toShort()
+inline val Char.ub get() = toUbyte()
+inline val Char.ui get() = toUint()
+inline val Char.ul get() = toUlong()
+inline val Char.us get() = toUshort()
+
+inline val Int.f get() = toFloat()
+inline val Int.b get() = toByte()
+inline val Int.d get() = toDouble()
+inline val Int.i get() = toInt()
+inline val Int.L get() = toLong()
+inline val Int.s get() = toShort()
+inline val Int.ub get() = toUbyte()
+inline val Int.ui get() = toUint()
+inline val Int.ul get() = toUlong()
+inline val Int.us get() = toUshort()
+inline val Int.c get() = toChar()
+inline val Int.bool get() = i != 0
+
+inline val Long.f get() = toFloat()
+inline val Long.b get() = toByte()
+inline val Long.d get() = toDouble()
+inline val Long.i get() = toInt()
+inline val Long.L get() = toLong()
+inline val Long.s get() = toShort()
+inline val Long.ub get() = toUbyte()
+inline val Long.ui get() = toUint()
+inline val Long.ul get() = toUlong()
+inline val Long.us get() = toUshort()
+inline val Long.c get() = toInt().toChar()
+inline val Long.bool get() = i != 0
+
+inline val Float.f get() = toFloat()
+inline val Float.b get() = toInt().toByte()
+inline val Float.d get() = toDouble()
+inline val Float.i get() = toInt()
+inline val Float.L get() = toLong()
+inline val Float.s get() = toInt().toShort()
+inline val Float.ub get() = toUbyte()
+inline val Float.ui get() = toUint()
+inline val Float.ul get() = toUlong()
+inline val Float.us get() = toUshort()
+inline val Float.c get() = toInt().toChar()
+inline val Float.bool get() = i != 0
+
+inline val Double.f get() = toFloat()
+inline val Double.b get() = toInt().toByte()
+inline val Double.d get() = toDouble()
+inline val Double.i get() = toInt()
+inline val Double.L get() = toLong()
+inline val Double.s get() = toInt().toShort()
+inline val Double.ub get() = toUbyte()
+inline val Double.ui get() = toUint()
+inline val Double.ul get() = toUlong()
+inline val Double.us get() = toUshort()
+inline val Double.c get() = toInt().toChar()
+inline val Double.bool get() = i != 0
+
+inline val Number.f get() = toFloat()
+inline val Number.b get() = toByte()
+inline val Number.d get() = toDouble()
+inline val Number.i get() = toInt()
+inline val Number.L get() = toLong()
+inline val Number.s get() = toShort()
+inline val Number.ub get() = toUbyte()
+inline val Number.ui get() = toUint()
+inline val Number.ul get() = toUlong()
+inline val Number.us get() = toUshort()
+inline val Number.c get() = toInt().toChar()
+inline val Number.bool get() = i != 0
 
 
-val Boolean.f get() = if (this) 1f else 0f
-val Boolean.b get() = if (this) 1.b else 0.b
-val Boolean.d get() = if (this) 1.0 else 0.0
-val Boolean.i get() = if (this) 1 else 0
-val Boolean.L get() = if (this) 1L else 0L
-val Boolean.s get() = if (this) 1.s else 0.s
-val Boolean.ub get() = if (this) 1.ub else 0.ub
-val Boolean.ui get() = if (this) 1.ui else 0.ui
-val Boolean.ul get() = if (this) 1.ul else 0.ul
-val Boolean.us get() = if (this) 1.us else 0.us
+inline val Boolean.f get() = if (this) 1f else 0f
+inline val Boolean.b get() = if (this) 1.b else 0.b
+inline val Boolean.d get() = if (this) 1.0 else 0.0
+inline val Boolean.i get() = if (this) 1 else 0
+inline val Boolean.L get() = if (this) 1L else 0L
+inline val Boolean.s get() = if (this) 1.s else 0.s
+inline val Boolean.ub get() = if (this) 1.ub else 0.ub
+inline val Boolean.ui get() = if (this) 1.ui else 0.ui
+inline val Boolean.ul get() = if (this) 1.ul else 0.ul
+inline val Boolean.us get() = if (this) 1.us else 0.us
 
 
-val Char.f get() = code.toFloat()
-val Char.b get() = code.toByte()
-val Char.d get() = code.toDouble()
-val Char.i get() = code
-val Char.L get() = code.toLong()
-val Char.s get() = code.toShort()
-val Char.ub get() = toUbyte()
-val Char.ui get() = toUint()
-val Char.ul get() = toUlong()
-val Char.us get() = toUshort()
-
-
-infix fun Char.shl(bits: Int) = i shl bits
-infix fun Char.shr(bits: Int) = i shr bits
-operator fun Char.compareTo(other: Int) = i.compareTo(other)
-operator fun Int.plus(b: Char) = this + b.i
-operator fun Int.minus(b: Char) = this - b.i
-infix fun Int.xor(b: Char) = this xor b.i
+inline infix fun Char.shl(bits: Int) = i shl bits
+inline infix fun Char.shr(bits: Int) = i shr bits
+inline operator fun Char.compareTo(other: Int) = i.compareTo(other)
+inline operator fun Int.plus(b: Char) = this + b.i
+inline operator fun Int.minus(b: Char) = this - b.i
+inline infix fun Int.xor(b: Char) = this xor b.i
 
 
 val Int.uc get() = (this % 256).c // TODO others
@@ -208,7 +285,7 @@ infix fun Long.xor(other: Int) = this xor other.L
 val String.f get() = toFloat()
 val String.b get() = toByte()
 val String.d get() = toDouble()
-val String.i get() = if (startsWith("0x")) java.lang.Integer.parseInt(substring(2), 16) else toInt()
+val String.i get() = if (startsWith("0x")) Integer.parseInt(substring(2), 16) else toInt()
 val String.L
     get() = try {
         if (startsWith("0x"))
@@ -218,6 +295,7 @@ val String.L
         bi.L
     }
 val String.s get() = toShort()
+
 // TODO unsigned String extensions
 val String.ub get() = Ubyte(this)
 val String.ui get() = Uint(this)
@@ -258,10 +336,11 @@ fun InputStream.long(bigEndian: Boolean = true): Long {
 }
 
 fun InputStream.mat4(bigEndian: Boolean = true) = Mat4(
-        float(bigEndian), float(bigEndian), float(bigEndian), float(bigEndian),
-        float(bigEndian), float(bigEndian), float(bigEndian), float(bigEndian),
-        float(bigEndian), float(bigEndian), float(bigEndian), float(bigEndian),
-        float(bigEndian), float(bigEndian), float(bigEndian), float(bigEndian))
+    float(bigEndian), float(bigEndian), float(bigEndian), float(bigEndian),
+    float(bigEndian), float(bigEndian), float(bigEndian), float(bigEndian),
+    float(bigEndian), float(bigEndian), float(bigEndian), float(bigEndian),
+    float(bigEndian), float(bigEndian), float(bigEndian), float(bigEndian)
+)
 
 internal val Any.toFloat: Float
     get() = when (this) {
