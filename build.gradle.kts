67df994d9d511b4f7eee8bc7e2134a21a2ec1964
Bixilon <bixilon@bixilon.de>
2023-06-04 00:07:52 +0200

    remove lwjgl as run dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 import magik.createGithubPublication
 import magik.github
 import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
-import org.lwjgl.lwjgl
-import org.lwjgl.Lwjgl.Module.*
 
 plugins {
     kotlin("jvm") version embeddedKotlinVersion
-    id("org.lwjgl.plugin") version "0.0.34"
     id("elect86.magik") version "0.3.2"
     `maven-publish`
 //    id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -20,7 +17,8 @@ repositories {
 dependencies {
     api("kotlin.graphics:unsigned:3.3.32")
     api("kotlin.graphics:kool:0.9.77")
-    // lwjgl { implementation(glfw, jemalloc, openal, opengl, stb) }
+    api("org.lwjgl:lwjgl-jemalloc:3.3.2")
+
 
     testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
     testImplementation("io.kotest:kotest-assertions-core:5.6.2")
